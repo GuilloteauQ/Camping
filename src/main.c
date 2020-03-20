5d652dddef9eb3bdae73344349ba5c1ed5adf050
Quentin Guilloteau <Quentin.Guilloteau@inria.fr>
2020-03-20 03:45:21 +0100

    queue

--- src/main.c
+++ src/main.c
@@ -39,7 +39,59 @@ void tell_him_i_am_ready(int destination) {
     MPI_Send(0, 1, MPI_INT, destination, 99, MPI_COMM_WORLD);
 }
 
-int get_index_free_node(int world_size) {
+struct queue {
+    int node;
+    struct queue* next;
+};
+
+void print_queue(struct queue* q) {
+    if (q != NULL) {
+        printf("%d->", q->node);
+        print_queue(q->next);
+    } else {
+        printf("NULL\n");
+    }
+}
+
+void push(struct queue** q, int node) {
+    // printf("PUSHING %d into: ", node);
+    // print_queue(*q);
+    struct queue* new_item = malloc(sizeof(struct queue*));
+    new_item->node = node;
+    new_item->next = NULL;
+    if (*q == NULL) {
+        *q = new_item;
+    } else {
+        struct queue* current = *q;
+        while (current->next != NULL) {
+            current = current->next;
+        }
+        current->next = new_item;
+    }
+    // printf("DONE PUSHING %d: ", node);
+    // print_queue(*q);
+}
+
+int pop(struct queue** q) {
+    // printf("POPPING FROM: ");
+    // print_queue(*q);
+    if (*q == NULL)
+        return -1;
+    int node = (*q)->node;
+    struct queue* new_head = (*q)->next;
+    free(*q);
+    *q = new_head;
+    // printf("POPPED %d, remaining: ", node);
+    // print_queue(*q);
+    // print_queue(new_head);
+    return node;
+}
+
+int get_index_free_node(int world_size, struct queue** q) {
+    int first_in_queue = pop(q);
+    if (first_in_queue != -1)
+        return first_in_queue;
+
     MPI_Request* reqs = malloc(sizeof(MPI_Request) * (world_size - 1));
     int* status = malloc(sizeof(int) * (world_size - 1));
 
@@ -49,27 +101,28 @@ int get_index_free_node(int world_size) {
                   &reqs[i - 1]);
     }
 
-    int i = 0;
-    while (1) {
-        MPI_Request_get_status(reqs[i], &flag, MPI_STATUS_IGNORE);
-        // printf("node: %d, flag: %d\n", i + 1, flag);
-        if (flag) {
-            if (status[i] == 0) {
-                free(status);
-                for (int j = 0; j < world_size - 1; j++)
-                    MPI_Request_free(&(reqs[j]));
-                return i + 1;
+    while (*q == NULL) {
+        for (int i = 1; i < world_size; i++) {
+            MPI_Request_get_status(reqs[i - 1], &flag, MPI_STATUS_IGNORE);
+            // printf("node: %d, flag: %d\n", i, flag);
+            if (flag) {
+                if (status[i - 1] == 0) {
+                    push(q, i);
+                }
             }
         }
-        i = (i + 1) % (world_size - 1);
-        if (i == 0)
-            sleep(10);
+        sleep(10);
     }
+    free(status);
+    for (int j = 0; j < world_size - 1; j++)
+        MPI_Request_free(&(reqs[j]));
+    return pop(q);
 }
 
 void start_master(char* filename) {
     int world_size = get_world_size();
     int ready_status = 0;
+    struct queue* q = NULL;
     FILE* fp;
     char* line = NULL;
     size_t len = 0;
@@ -82,7 +135,8 @@ void start_master(char* filename) {
     while ((read = getline(&line, &len, fp)) != -1) {
         // printf("Retrieved line of length %zu:\n", read);
         // printf("%s", line);
-        int free_node = get_index_free_node(world_size);
+        int free_node = get_index_free_node(world_size, &q);
+        print_queue(q);
         printf("[MASTER] Running on node %d ('%s')\n", free_node, line);
         MPI_Send(&ready_status, 1, MPI_INT, free_node, 99, MPI_COMM_WORLD);
         send_string(line, free_node);
@@ -144,17 +198,17 @@ void tell_the_boss_i_am_busy() {
     tell_boss(-1);
 }
 
-void start_slave() {
+void start_slave(int my_rank) {
     tell_the_boss_i_am_free();
     int status;
     MPI_Recv(&status, 1, MPI_INT, 0, 99, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     if (status == 0) {
         // tell_the_boss_i_am_busy();
-        printf("[Node %d] Executing job\n", get_my_rank());
+        printf("[Node %d] Executing job\n", my_rank);
         slave_execute_command();
-        start_slave();
+        start_slave(my_rank);
     } else {
-        printf("[Node %d] Going back home !\n", get_my_rank());
+        printf("[Node %d] Ok ! Going back home !\n", my_rank);
     }
 }
 
@@ -169,7 +223,7 @@ int main(int argc, char** argv) {
     if (my_rank == 0) {
         start_master(argv[1]);
     } else {
-        start_slave();
+        start_slave(my_rank);
     }
 
     MPI_Finalize();
